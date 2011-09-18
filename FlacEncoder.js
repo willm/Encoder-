a2ee45cbe95b787f22ba563d39212c64f6bd1ae5
Will Munn <willmunn@hotmail.com>
2011-09-18 11:34:50 +0100

    encode files and remove temp wav

--- FlacEncoder.js
+++ FlacEncoder.js
@@ -1,5 +1,5 @@
 require('./StringExtensions');
-var nexpect = require('nexpect'),
+var exec = require('child_process').exec,
 	step = require('step');
 
 function Encoder() {};
@@ -12,8 +12,14 @@ Encoder.prototype.encode = function(filepath){
 			  that.decodeFlac(filepath,this);
 			},
 			function () {
-			  console.log('hi ther eeeee');
-			});
+			  that.convertToMp3('test.wav', 320,this);
+			  that.convertToMp3('test.wav', 128,this);
+			  that.convertToMp3('test.wav', 256,this);
+			},
+			function () {
+			  exec('rm test.wav', {cwd:'spec/res'}, this);
+			}
+			);
 		}
 	else{
 		throw{
@@ -23,27 +29,42 @@ Encoder.prototype.encode = function(filepath){
 	}
 };
 
-Encoder.prototype.decodeFlac = function(filepath){
-		nexpect.spawn('ffmpeg -y -i '+filepath + ' ' + 'test.wav')
-			.expect()
-			.run(function (err) {
-				if (err) {
-					console.log(err);
-				}
-				console.log('success');
+Encoder.prototype.decodeFlac = function(filepath, cb){
+		var command = 'ffmpeg -y -i ' +
+						filepath + ' ' +
+						filepath.replaceExtension('.wav');
+						
+		exec(command,
+			function (error, stout, sterr) {
+			  console.log('stout: ' + stout);
+			  console.log('sterr: ' + sterr);
+			  if(error !== null){
+				  throw{
+					name:'FlacDecodeError',
+					message:error
+				};
+			  }
+			  cb();
 			});
 };
 
-Encoder.prototype.compressDecodedFlac = function(){
-		console.log('grgbsdfbvsdfbsdfbsdfbdfbfdbsdfbdf');
-		nexpect.spawn('lame test.wav test.mp3', {verbose:true})
-			.run(function (err) {
-				if (err) {
-					console.log(err);
-				}
-				console.log('success');
+Encoder.prototype.convertToMp3 = function(inputFile, bitrate, cb){
+		var command = 'lame -b ' + bitrate + ' ' 
+			+ inputFile + ' ' + 
+			inputFile.replaceExtension('_'+bitrate+'.mp3');
+		exec(command, {cwd:'spec/res'},
+			function (error, stout, sterr) {
+			  console.log('stout: ' + stout);
+			  console.log('sterr: ' + sterr);
+			  if (error !== null) {
+			    throw{
+			    	name:'LameError',
+			    	message:error
+			    };
+			  }
+			cb();
 			});
-}
+};
 
 var e = new Encoder();
 e.encode('spec/res/test.flac');
--- StringExtensions.js
+++ StringExtensions.js
@@ -2,3 +2,6 @@ String.prototype.endsWith = function(suffix) {
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
+String.prototype.replaceExtension = function (newExtension) {
+  return this.substring(0, this.lastIndexOf('.')) + newExtension;
+}
