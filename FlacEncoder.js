81f684a578494204a7f5f3843ca767142635b2d3
willm <william.munn@7digital.com>
2011-09-19 18:46:37 +0100

    added paths to encoders

--- FlacEncoder.js
+++ FlacEncoder.js
@@ -7,6 +7,9 @@ var exec = require('child_process').exec,
 function FlacEncoder(inputDirectiory) {
 	this.inDir = inputDirectiory;
 	this.outDir = path.join(inputDirectiory,'out');
+	this.ffmpeg = path.join(__dirname,'lib','ffmpeg-0.5', 'ffmpeg.exe');
+	this.lame = path.join(__dirname,'lib','lame3.98.4', 'lame.exe');
+	this.nero = path.join(__dirname,'lib','nero1.5.1', 'win32', 'neroAacEnc.exe');
 };
 
 FlacEncoder.prototype.encode = function(filepath){
@@ -58,7 +61,7 @@ FlacEncoder.prototype.executeCommand = function (command, err,cb) {
 };
 
 FlacEncoder.prototype.decodeFlac = function(filename, cb){
-		var command = 'ffmpeg -y -i ' +
+		var command =  this.ffmpeg +' -y -i ' +
 						filename + ' ' +
 						filename.replaceExtension('.wav');
 		console.log(command + ' : ' + this.inDir);
@@ -66,14 +69,14 @@ FlacEncoder.prototype.decodeFlac = function(filename, cb){
 };
 
 FlacEncoder.prototype.convertToMp3 = function(inputFile, bitrate, cb){
-		var command = 'lame -b ' + bitrate + ' ' 
+		var command = this.lame +' -b ' + bitrate + ' ' 
 			+ inputFile + ' ' + 
 			path.join(this.outDir,inputFile.replaceExtension('_'+bitrate+'.mp3'));
 		this.executeCommand(command, 'Mp3EncodeError', cb);
 };
 
 FlacEncoder.prototype.clipMp3 = function(inputFile, cb){
-		var command = 'ffmpeg -ss 0 -t 30 -i ' + path.join(this.outDir,inputFile) + ' -acodec copy ' + 
+		var command = this.ffmpeg +' -ss 0 -t 30 -i ' + path.join(this.outDir,inputFile) + ' -acodec copy ' + 
 			path.join(this.outDir,inputFile.replaceExtension('_mp3clip.mp3'));
 			console.log("COMMAND: " + command);
 		this.executeCommand(command, 'ClippingError', cb);
@@ -82,7 +85,7 @@ FlacEncoder.prototype.clipMp3 = function(inputFile, cb){
 FlacEncoder.prototype.convertToM4A = function(inputFile, bitrate, cb){
 		var kbpsinbps =  0.0009765625,
 			bitrate = bitrate/kbpsinbps,
-			command = 'wine ../../lib/win32/neroAacEnc.exe -br ' + bitrate + ' ' +
+			command = this.nero+' -br ' + bitrate + ' ' +
 				'-if ' + inputFile + ' ' + 
 				'-of ' + path.join(this.outDir,inputFile.replaceExtension('.m4a'));
 		this.executeCommand(command, 'M4AEncodeError', cb);
